ccea5bf8917ecc9b0a1bd10491d7ceb241cab9e0
AUR.WILKOSZ <AUR.WILKOSZ@s124-9.pedago.mermoz.edu>
2019-11-04 15:35:41 +0100

    Exercice 4

---
+++ src/tp3/Tp3Exo3.java
@@ -0,0 +1,15 @@
+package tp3;
+
+public class Tp3Exo3 {
+
+	 public static void main(String[] args)   {   
+		 int n=7,p=4;   
+		 int negatif = -32000; 
+	 
+	  System.out.println("n&p,n|p,n^p = " + (n&p) +" "+ (n|p) + " " + (n^p));   
+	  System.out.println("n>>2,n>>4,n<<2 = " + (n>>2) +" "+ (n>>4) + " " + (n<<2)); 
+	 
+	  System.out.println("negatif>>2, negatif>>4 = " + (negatif>>2) +" "+(negatif>>4));   
+	  System.out.println("negatif>>>2, negatif>>>4 = " + (negatif>>>2) +" "+ (negatif>>>4));   } 
+
+}
---
+++ src/tp3/Tp3Exo4.java
@@ -0,0 +1,24 @@
+package tp3;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Tp3Exo4 {
+
+	public static void main(String[] args) {
+		Scanner saisie = new Scanner(System.in);
+		
+		String heures = JOptionPane.showInputDialog(null, "Saisir les heures :");
+		String minutes = JOptionPane.showInputDialog(null, "Saisir les minutes:");
+		String secondes = JOptionPane.showInputDialog(null, "Saisir les secondes:");
+		
+		int h = Integer.parseInt(heures);
+		int m = h * 60 + Integer.parseInt(minutes);
+		int s = Integer.parseInt(secondes);
+
+		JOptionPane.showMessageDialog(null, "Résultat = "+ (m * 60 + s));
+		saisie.close();
+
+	}
+
+}
