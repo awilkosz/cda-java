0b112b6f8eb059ebad705c451307ea136fc997ec
Senga777 <agnes.needemand@gmail.com>
2019-11-04 13:23:14 +0100

    change struture

---
+++ src/tp1/Tp1Exo1.java
@@ -0,0 +1,9 @@
+package tp1;
+
+public class Tp1Exo1 {
+
+	public static void main(String[] args) {
+		System.out.print("TP1");
+	}
+
+}
---
+++ src/tp2/Tp2Exo1.java
@@ -0,0 +1,9 @@
+package tp2;
+
+public class Tp2Exo1 {
+
+	public static void main(String[] args) {
+		System.out.print("TP2");
+	}
+
+}
