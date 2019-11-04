3b1c80377e91064ac2ed4f7934af1495e0fe2b5f
AUR.WILKOSZ <AUR.WILKOSZ@s124-9.pedago.mermoz.edu>
2019-11-04 15:53:33 +0100

    Début TP4

---
+++ src/tp4/Tp4Exo1.java
@@ -0,0 +1,25 @@
+package tp4;
+
+import java.util.Scanner;
+
+public class Tp4Exo1 {
+
+	public static void main(String[] args) {
+		Scanner saisie = new Scanner(System.in);
+		
+		System.out.print("Combien de notes ?");
+		int nbNotes = saisie.nextInt();
+		double moyenne = 0;
+		
+		for(int i = 0; i < nbNotes; i++)
+		{
+			System.out.println("Note " + (i+1));
+			double note = saisie.nextDouble();
+			moyenne += note;
+		}
+		
+		System.out.println("moyenne de ces 4 notes : " + (moyenne / 4));
+			
+	}
+
+}
