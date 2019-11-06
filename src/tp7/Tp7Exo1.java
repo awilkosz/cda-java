254c47e5edf2bde5708a5e3fd97c88d035dc24d0
AUR.WILKOSZ <AUR.WILKOSZ@s124-9.pedago.mermoz.edu>
2019-11-06 15:27:14 +0100

    Exercices terminé mais sans le pseudo code

---
+++ src/tp7/Tp7Exo1.java
@@ -0,0 +1,26 @@
+package tp7;
+
+import java.util.Scanner;
+
+public class Tp7Exo1 {
+
+	public static void main(String[] args) {
+		Scanner saisie = new Scanner(System.in);
+		System.out.println("Saisir le rayon du cercle : ");
+		int r = saisie.nextInt();
+		
+		System.out.println(surface(r));
+		System.out.println(perimetre(r));
+		saisie.close();
+	}
+
+	public static double surface(int rayon)
+	{
+		return Math.PI * Math.pow(rayon, 2);
+	}
+	
+	public static double perimetre(int rayon)
+	{
+		return 2 * Math.PI * rayon;
+	}
+}
---
+++ src/tp7/Tp7Exo2.java
@@ -0,0 +1,28 @@
+package tp7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Tp7Exo2 {
+
+	public static void main(String[] args) {
+		Scanner saisie = new Scanner(System.in);
+		
+		System.out.println("Le premier nombre entier : ");
+		int nb1 = saisie.nextInt();
+		System.out.println("Le deuxième nombre entier : ");
+		int nb2 = saisie.nextInt();
+		System.out.println("Le troisième nombre entier : ");
+		int nb3 = saisie.nextInt();
+		
+		System.out.println(plusGrand(nb1,nb2,nb3));
+		saisie.close();
+	}
+
+	public static int plusGrand(int a, int b, int c)
+	{
+		int tab[] = {a, b, c};  
+		Arrays.sort(tab);
+		return tab[2];
+	}
+}
